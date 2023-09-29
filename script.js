a951558e572469959adb785cd24f1fd96c3572a4
Manish Sethia <Manish.Sethia.Contractor@pepsico.com>
2023-09-30 00:09:37 +0530

    Changes to support server side changes

--- script.js
+++ script.js
@@ -1,19 +1,11 @@
-// Import the recordrtc library
-
-//import RecordRTC from 'recordrtc';
-
-let recorder;
-
 function convertArticleToMP3() {
   const articleUrl = document.getElementById('articleUrl').value;
 
   fetch(`https://listenify-service.onrender.com/fetch?url=${encodeURIComponent(articleUrl)}`)
-    .then(response => response.text())
-    .then(text => {
-      // Convert text to audio using your existing function (textToSpeech)
-      const audioBlob = textToSpeech(text);
-
-      // Create a download link for the audio
+    .then(response => response.arrayBuffer())
+    .then(audioData => {
+      // Convert audioData to a Blob and create a download link
+      const audioBlob = new Blob([audioData], { type: 'audio/wav' });
       const downloadLink = document.createElement('a');
       downloadLink.href = URL.createObjectURL(audioBlob);
       downloadLink.download = 'article.mp3';
@@ -22,36 +14,4 @@ function convertArticleToMP3() {
       document.body.removeChild(downloadLink);
     })
     .catch(error => console.error('Error fetching the article:', error));
-}
-
-// Function to convert text to audio using recordrtc
-function textToSpeech(text) {
-  const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-  const source = audioContext.createBufferSource();
-
-  // Initialize recorder
-  recorder = new RecordRTC(source, {
-    type: 'audio',
-    mimeType: 'audio/wav', // You can change the mimeType to 'audio/mp3'
-  });
-
-  // Convert text to audio
-  const speechSynthesis = window.speechSynthesis;
-  const utterance = new SpeechSynthesisUtterance(text);
-  utterance.rate = 1; // Adjust the speech speed if needed
-  utterance.voice = speechSynthesis.getVoices()[0]; // Set the voice (optional)
-  utterance.onend = function() {
-    recorder.stopRecording(function() {
-      const blob = recorder.getBlob();
-      source.disconnect();
-      audioContext.close();
-    });
-  };
-
-  // Start recording
-  recorder.startRecording();
-  speechSynthesis.speak(utterance);
-
-  // Return the audio blob
-  return recorder.getBlob();
 }
