ac698812987f539417c341eed90f38e5a4dfcb65
Manish Sethia <Manish.Sethia.Contractor@pepsico.com>
2023-09-30 01:16:05 +0530

    Changes to convert to wav file

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
+const wavEncoder = require('wav-encoder');
+
 function convertArticleToWAV() {
-  const articleUrl = document.getElementById('articleUrl').value;
+    const articleUrl = document.getElementById('articleUrl').value;
 
-  fetch(`https://listenify-service.onrender.com/fetch?url=${encodeURIComponent(articleUrl)}`)
-      .then(response => response.arrayBuffer())
-      .then(audioData => {
-          const audioBlob = new Blob([audioData], { type: 'audio/wav' });
-          const audioUrl = URL.createObjectURL(audioBlob);
-          const audioElement = document.createElement('audio');
-          audioElement.src = audioUrl;
-          audioElement.controls = true;
+    fetch(`https://listenify-service.onrender.com/fetch?url=${encodeURIComponent(articleUrl)}`)
+        .then(response => response.arrayBuffer())
+        .then(audioData => {
+            // Convert audio data to WAV format
+            wavEncoder.encode({
+                sampleRate: 44100, // Adjust as needed
+                channelData: [new Float32Array(audioData)]
+            }).then((buffer) => {
+                const audioBlob = new Blob([buffer], { type: 'audio/wav' });
+                const audioUrl = URL.createObjectURL(audioBlob);
 
-          const resultDiv = document.getElementById('result');
-          resultDiv.innerHTML = '';
-          resultDiv.appendChild(audioElement);
-      })
-      .catch(error => console.error('Error fetching the article:', error));
+                // Create a download link
+                const downloadLink = document.createElement('a');
+                downloadLink.href = audioUrl;
+                downloadLink.download = 'article.wav';
+                document.body.appendChild(downloadLink);
+                downloadLink.click();
+                document.body.removeChild(downloadLink);
+            });
+        })
+        .catch(error => console.error('Error fetching the article:', error));
 }
